8adf5a6ea24cb99ac4511d324f802027c10c882b
유현승 <gustmd5715@gmail.com>
2026-02-09 17:33:26 +0900

    [D3] Title: 햄버거 다이어트, Time: 155 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -17,53 +17,83 @@ public class Solution {
 
 		for(int t = 1; t <= T; t++)
 		{
-			int solve = solution();
-			sb.append("#" + t + " " + solve + "\n");
+			int solution = solution();
+			sb.append("#" + t + " " + solution + "\n");
 		}
 
 		System.out.println(sb.toString());
 		br.close();
 	}
 
-	static int N, L, max;
+	static int N, L;
 	static int[] scores;
 	static int[] calories;
+	static int max = 0;
 
 	static int solution() throws Exception {
-		st = new StringTokenizer(br.readLine(), " ");
+		max = 0;
+
+		st= new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		L = Integer.parseInt(st.nextToken());
+
 		scores = new int[N];
 		calories = new int[N];
-
-		max = 0;
 		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
+			st= new StringTokenizer(br.readLine(), " ");
+
 			scores[i] = Integer.parseInt(st.nextToken());
 			calories[i] = Integer.parseInt(st.nextToken());
 		}
 
-		dfs(0, 0, 0);
+		for (int i = 1; i <= N; i++) {
+			int[] comb = new int[i];
+			combination(comb, 0, 0, new boolean[N], i);
+		}
 
 		return max;
 	}
 
-	static void dfs(int index, int sumScore, int sumCalorie) {
-		// 가지치기, 칼로리가 L을 넘었는지
-		if(L < sumCalorie)
-			return;
-
-		if(index == N) {
-			max = Math.max(max, sumScore);
+	static void combination(int[] comb, int depth, int start, boolean[] visited, int R) {
+		if (depth == R) {
+			// 계산
+			int score = calc(comb);
+			max = Math.max(max, score);
 			return;
 		}
-		max = Math.max(max, sumScore);
 
-		dfs(index + 1, sumScore, sumCalorie);
-		dfs(index + 1, sumScore + scores[index], sumCalorie + calories[index]);
+		for (int i = start; i < N; i++) {
+			if(visited[i])
+				continue;
+
+			visited[i] = true;
+			comb[depth] = i;
+			combination(comb, depth+1, i, visited, R);
+			visited[i] = false;
+		}
 	}
 
+	static boolean isDuplicate(int[] comb, int count, int i) {
+		for (int j = 0; j < count; j++) {
+			if(comb[j] == i)
+				return true;
+		}
 
+		return false;
+	}
 
+	static int calc(int[] comb) {
+		int sumScores = 0;
+		int sumCalories = 0;
 
+		for (int i = 0; i < comb.length; i++) {
+			sumScores += scores[comb[i]];
+			sumCalories += calories[comb[i]];
+		}
+
+		if(sumCalories <= L)
+			return sumScores;
+
+		return 0;
+	}
 }
