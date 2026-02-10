1563b086d778096e3ac54da3a99be30e86df38c0
유현승 <gustmd5715@gmail.com>
2026-02-10 09:32:09 +0900

    [D3] Title: N-Queen, Time: 90 ms, Memory: 26,624 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -62,45 +62,31 @@ public class Solution {
 			// 일단 가로 검사 -> depth 겹치는 게 있나
 			// 그리고 세로 검사 -> i가 같고, dpeth만 다르게 겹치는 게 있나
 			// 그리고 대각선 검사 -> i가 같고, dpeth만 다르게 겹치는 게 있나
-			boolean isDiagonalDuplicate = isDiagonalDuplicate(depth, i);
+			int rightDownIndex = N - (depth - i) - 1;
+			int leftDownIndex = depth + i;
 
 			// 하나라도 겹치면(가로, 세로, 대각선) continue
-			if(garo[depth] || sero[i] || isDiagonalDuplicate)
+			if(garo[depth] || sero[i] || rightDown[rightDownIndex] || leftDown[leftDownIndex])
 				continue;
 
 			// [depth][i]에 퀸을 뒀으니까 상하좌우, 대각선에 뒀다는 걸 알리자 -> 1. 이걸 근데 둘 때마다 검사하는 것. 2. 행/열에 둘 수 있는지 검증하는 배열을 하나 두는 것?
 			board[depth][i] = true;
 			garo[depth] = true;
 			sero[i] = true;
+			rightDown[rightDownIndex] = true;
+			leftDown[leftDownIndex] = true;
 
 			choose(depth+1);
 
 			board[depth][i] = false;
 			garo[depth] = false;
 			sero[i] = false;
+			rightDown[rightDownIndex] = false;
+			leftDown[leftDownIndex] = false;
 
 		}
 
 	}
-
-	static boolean isDiagonalDuplicate(int i, int j) {
-		boolean isDiagonalDuplicate = false;
-		for (int r = 0; r < N; r++) {
-			for (int k = 0; k < 4; k++) {
-				int nI = i + dI[k] * r;
-				int nJ = j + dJ[k] * r;
-
-				if(!isAvailable(nI, nJ))
-					continue;
-
-				if(board[nI][nJ])
-					isDiagonalDuplicate = true;
-			}
-		}
-
-		return isDiagonalDuplicate;
-	}
-
 
 	static boolean isAvailable(int i, int j) {
 		if(i < 0 || N<=i || j < 0 || N<=j)
