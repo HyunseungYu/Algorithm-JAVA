4cefbd591c22986ad07c53b6022d7bb5e30cd0b2
gustmd5715 <gustmd5715@gmail.com>
2026-02-11 16:19:36 +0900

    [SWEA] 1486 제출

---
+++ src/main/java/SWEA/Solution_d4_1486_장훈이의높은선반_서울_8반_유현승.java
@@ -0,0 +1,72 @@
+package SWEA;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution_d4_1486_장훈이의높은선반_서울_8반_유현승 {
+
+	static BufferedReader br;
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+//		System.setIn(new FileInputStream("res/input.txt"));
+		br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		for(int t = 1; t <= T; t++) {
+			int solution = solution();
+			sb.append("#" + t + " " + solution + "\n");
+		}
+
+		System.out.println(sb.toString());
+		br.close();
+	}
+
+	static int N, H;
+	static int[] heights;
+	static int min;
+
+	static int solution() throws Exception {
+		min = Integer.MAX_VALUE;
+
+		st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
+
+		heights = new int[N];
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++)
+			heights[i] = Integer.parseInt(st.nextToken());
+
+		Arrays.sort(heights);
+		choose(0, 0);
+
+		return min;
+	}
+
+	static void choose(int depth, int heightSum) {
+
+		if(H <= heightSum) {
+			min = Math.min(min, heightSum - H);
+			return;
+		}
+
+		if(depth == N) {
+			if(H <= heightSum)
+				min = Math.min(min, heightSum - H);
+			return;
+		}
+
+
+		// 현재 사람 탑 쌓기 O
+		choose(depth + 1, heightSum + heights[depth]);
+
+		// 현재 사람 탑 쌓기 X
+		choose(depth + 1, heightSum);
+
+	}
+}
