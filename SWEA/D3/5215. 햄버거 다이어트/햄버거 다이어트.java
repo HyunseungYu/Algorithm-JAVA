c2321c4032896c1e02aa770663a5beb12672c300
유현승 <gustmd5715@gmail.com>
2026-02-16 17:08:27 +0900

    [D3] Title: 햄버거 다이어트, Time: 155 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,99 +1,58 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
+import java.io.*;
+import java.util.*;
 
 public class Solution {
 
-	static BufferedReader br;
-	static StringTokenizer st;
-	static StringBuilder sb = new StringBuilder();
+    static StringBuilder sb = new StringBuilder();
+    static Scanner sc;
+    static BufferedReader br;
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 //		System.setIn(new FileInputStream("res/input.txt"));
-		br = new BufferedReader(new InputStreamReader(System.in));
+        sc = new Scanner(System.in);
+//        br = new BufferedReader(new InputStreamReader(System.in));
 
-		int T = Integer.parseInt(br.readLine());
+//        int T = Integer.parseInt(br.readLine());
+        int T = sc.nextInt();
 
-		for(int t = 1; t <= T; t++)
-		{
-			int solution = solution();
-			sb.append("#" + t + " " + solution + "\n");
-		}
+        for(int t = 1; t <= T; t++) {
+            int solution = solve();
+            sb.append("#" + t + " " + solution + "\n");
+        }
 
-		System.out.println(sb.toString());
-		br.close();
-	}
+        System.out.println(sb.toString());
+    }
 
-	static int N, L;
-	static int[] scores;
-	static int[] calories;
-	static int max = 0;
 
-	static int solution() throws Exception {
-		max = 0;
+    static int solve() throws Exception {
+        int N = sc.nextInt();
+        int L = sc.nextInt();
 
-		st= new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		L = Integer.parseInt(st.nextToken());
+        int[] scores = new int[N+1];
+        int[] calories = new int[N+1];
 
-		scores = new int[N];
-		calories = new int[N];
-		for (int i = 0; i < N; i++) {
-			st= new StringTokenizer(br.readLine(), " ");
+        for (int i = 1; i <= N; i++) {
+            scores[i] = sc.nextInt();
+            calories[i] = sc.nextInt();
+        }
 
-			scores[i] = Integer.parseInt(st.nextToken());
-			calories[i] = Integer.parseInt(st.nextToken());
-		}
+        int[][] dp = new int[N+1][L+1];
+        for (int i = 1; i <= N; i++) {
+            for (int c = 0; c <= L; c++) {
+                dp[i][c] = dp[i-1][c];
+                if(c >= calories[i]) {
+                    dp[i][c] = Math.max(dp[i][c], dp[i-1][c-calories[i]] + scores[i]);
+                }
+                
+            }
 
-		for (int i = 1; i <= N; i++) {
-			int[] comb = new int[i];
-			combination(comb, 0, 0, new boolean[N], i);
-		}
+        }
 
-		return max;
-	}
 
-	static void combination(int[] comb, int depth, int start, boolean[] visited, int R) {
-		if (depth == R) {
-			// 계산
-			int score = calc(comb);
-			max = Math.max(max, score);
-			return;
-		}
+        return dp[N][L];
 
-		for (int i = start; i < N; i++) {
-			if(visited[i])
-				continue;
 
-			visited[i] = true;
-			comb[depth] = i;
-			combination(comb, depth+1, i, visited, R);
-			visited[i] = false;
-		}
-	}
+    }
 
-	static boolean isDuplicate(int[] comb, int count, int i) {
-		for (int j = 0; j < count; j++) {
-			if(comb[j] == i)
-				return true;
-		}
-
-		return false;
-	}
-
-	static int calc(int[] comb) {
-		int sumScores = 0;
-		int sumCalories = 0;
-
-		for (int i = 0; i < comb.length; i++) {
-			sumScores += scores[comb[i]];
-			sumCalories += calories[comb[i]];
-		}
-
-		if(sumCalories <= L)
-			return sumScores;
-
-		return 0;
-	}
 }
