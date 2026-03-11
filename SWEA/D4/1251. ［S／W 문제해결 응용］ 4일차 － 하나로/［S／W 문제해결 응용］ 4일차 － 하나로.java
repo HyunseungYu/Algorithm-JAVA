705fa8d6b90e94da36b458287087ce40ad70f2ef
유현승 <gustmd5715@gmail.com>
2026-03-11 09:21:54 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 329 ms, Memory: 119,712 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -26,52 +24,38 @@ public class Solution {
 	}
 
 	static int N;
-	static long[][] cord;
+	static int[][] cords;
 	static long[][] costs;
 
 	static long solve() throws Exception {
 		N = sc.nextInt();
 
-		cord = new long[N][2];
-		for (int i = 0; i < N; i++) {
-			long x = sc.nextLong();
-			cord[i] = new long[] {x, 0};
-		}
-		for (int i = 0; i < N; i++) {
-			long y = sc.nextLong();
-			cord[i][1] = y;
-		}
+		cords = new int[N][N];
+		for (int i = 0; i < N; i++)
+			cords[i] = new int[] {sc.nextInt(), 0};
+
+		for (int i = 0; i < N; i++)
+			cords[i][1] = sc.nextInt();
+
 		double ratio = sc.nextDouble();
 
-
-		// 노드끼리 거리(코스트) 계산
-		List<long[]>[] G = new List[N];
-		for (int i = 0; i < N; i++) {
-			G[i] = new ArrayList<>();
-		}
-
+		costs = new long[N][N];
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if(i == j)
-					continue;
-
-				G[i].add(new long[] {j, getDistance(cord[i], cord[j])});
+				costs[i][j] = getDistance(cords[i], cords[j]);
 			}
 		}
-
 
 		boolean[] visited = new boolean[N];
 		long[] P = new long[N];
 		for (int i = 0; i < N; i++)
 			P[i] = Long.MAX_VALUE;
-		P[0] = 0;
 
 		long mst = 0;
-		int count = 0;
+		P[0] = 0;
 		for (int i = 0; i < N; i++) {
 			long min = Long.MAX_VALUE;
-			int minVertex = -1;
-
+			long minVertex = -1;
 			for (int j = 0; j < N; j++) {
 				if(!visited[j] && P[j] < min) {
 					min = P[j];
@@ -79,29 +63,28 @@ public class Solution {
 				}
 			}
 
-			visited[minVertex] = true;
+			visited[(int) minVertex] = true;
 			mst += min;
 
-			for (long[] v : G[minVertex]) {
-				if(!visited[(int) v[0]] && v[1] < P[(int) v[0]]) {
-					P[(int) v[0]] = v[1];
+			for (int j = 0; j < N; j++) {
+				if(minVertex == j)
+					continue;
+
+				long cost = costs[(int) minVertex][j];
+				if(!visited[j] && cost < P[j]) {
+					P[j] = cost;
 				}
 			}
-
 		}
 
-		return (long) (Math.round(mst * ratio));
+		return Math.round(mst * ratio);
 	}
 
-	static long getDistance(long[] p1, long[] p2) {
-		long p1X = p1[0];
-		long p1Y = p1[1];
+	static long getDistance(int[] cord1, int[] cord2) {
+		long width = cord1[0] - cord2[0];
+		long height = cord1[1] - cord2[1];
 
-		long p2X = p2[0];
-		long p2Y = p2[1];
-
-		return (p1X - p2X) * (p1X - p2X) + (p1Y - p2Y) * (p1Y - p2Y);
+		return width * width + height * height;
 	}
-
 
 }
