e36d0e8daa02db3af2b0b0dbb3e55438f3605ec0
유현승 <gustmd5715@gmail.com>
2026-03-18 16:58:23 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 487 ms, Memory: 50,392 KB -BaekjoonHub

--- SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
+++ SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
@@ -37,18 +37,20 @@ public class Solution {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				field[i][j] = sc.nextInt();
-				if(field[i][j] == 1) {
+
+				if(field[i][j] == 1)
 					homes.add(new int[] {i, j});
-				}
 			}
 		}
 
-		int maxHomeCount = Integer.MIN_VALUE;
+		int maxHomeCount = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				for (int k = 1; k <= 2 * N; k++) {
-					int cost = k * k + (k-1) * (k-1);
-					int homeCount = getHomeInRange(i, j, k);
+
+				// 거리별로 home 개수 구하기
+				for (int k = 1; k < 2 * N; k++) {
+					int homeCount = getHomeCount(i, j, k);
+					int cost = k * k + (k - 1) * (k - 1);
 
 					if(cost <= homeCount * M)
 						maxHomeCount = Math.max(maxHomeCount, homeCount);
@@ -59,26 +61,16 @@ public class Solution {
 		return maxHomeCount;
 	}
 
-	static int getHomeInRange(int i, int j, int k) {
+	static int getHomeCount(int i, int j, int k) {
 		int count = 0;
 
 		for(int[] home : homes) {
-			int distance = Math.abs(home[0] - i) + Math.abs(home[1] - j);
+			int distance = Math.abs(i - home[0]) + Math.abs(j - home[1]);
+
 			if(distance < k)
 				count++;
 		}
 
 		return count;
-	}
-
-	static final int[] di = {-1, 1, 0, 0};
-	static final int[] dj = {0, 0, -1, 1};
-
-
-	static boolean isOk(int i, int j) {
-		if(i < 0 || N<=i || j < 0 || N<=j)
-			return false;
-
-		return true;
 	}
 }
