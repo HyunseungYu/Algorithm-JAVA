a0087e81b4c494c7f283d980c548dc570a7be437
유현승 <gustmd5715@gmail.com>
2026-04-13 16:14:52 +0900

    Time: 19 ms (23.17%) | Memory: 45.5 MB (71.77%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,25 +1,50 @@
 class Solution {
+    int TARGET;
+    int CLOSEST;
+
     public int threeSumClosest(int[] nums, int target) {
+        TARGET = target;
+        CLOSEST = Integer.MAX_VALUE / 2;
+
         int length = nums.length;
 
-        int closest = Integer.MAX_VALUE/2;
+        Arrays.sort(nums);
+        
+        for(int i = 1; i < length - 1; i++) {
+            int l = i - 1;
+            int r = i + 1;
 
-        for(int i=0; i<length; i++) {
-            for(int j=i+1; j<length; j++) {
-                for(int k=j+1; k<length; k++) {
-                    int sum = nums[i] + nums[j] + nums[k];
+            while(0 <= l && r < length) {
+                int left = nums[l];
+                int right = nums[r];
+                
+                int sum = nums[i] + left + right;
 
-                    int diff = target - sum;
-                    int closestDiff = target - closest;
+                if(sum == target)
+                    return target;
 
-                    if(Math.abs(diff) < Math.abs(closestDiff)) {
-                        // System.out.println("sum = " + sum + ", diff = " + diff + ", closestDiff = " + closestDiff);
-                        closest = sum;
-                    }
+                // sum이 target보다 작으면 -> r 한 칸 증가
+                if(sum < target) {
+                    comapre(sum);
+
+                    r++;
+                } else { // sum이 target보다 크면 -> l 한 칸 감소
+                    comapre(sum);
+
+                    l--;
                 }
             }
-        }   
+        }
 
-        return closest;
+        return CLOSEST;
+    }
+
+    private void comapre(int sum) {
+        int diff = TARGET - sum;
+        int closestDiff = TARGET - CLOSEST;
+
+        if(Math.abs(diff) < Math.abs(closestDiff)) {
+            CLOSEST = sum;
+        }
     }
 }
