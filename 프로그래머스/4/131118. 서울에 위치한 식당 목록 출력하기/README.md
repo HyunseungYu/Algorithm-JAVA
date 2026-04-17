11d64dd918bbb2099037675ccd128cfc3b5b3b3a
유현승 <gustmd5715@gmail.com>
2026-04-17 23:55:55 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/README.md
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/README.md
@@ -1,6 +1,6 @@
 # [level 4] 서울에 위치한 식당 목록 출력하기 - 131118 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131118) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131118?language=oracle) 
 
 ### 성능 요약
 
@@ -12,11 +12,11 @@
 
 ### 채점결과
 
-Empty
+합계: 100.0 / 100.0
 
 ### 제출 일자
 
-2026년 02월 20일 17:18:27
+2026년 04월 17일 23:55:51
 
 ### 문제 설명
 
--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,24 +1,13 @@
-# select
-#     info.REST_ID,
-#     REST_NAME,
-#     FOOD_TYPE,
-#     FAVORITES,
-#     ADDRESS,
-#     ROUND(AVG(review.REVIEW_SCORE)) as SCORE
-# from REST_INFO info
-# join REST_REVIEW review on info.REST_ID = review.REST_ID
-# group by info.REST_ID
-# where LEFT(info.ADDRESS, 2) = '서울';
-
-select
-    info.REST_ID,
-    REST_NAME,
-    FOOD_TYPE,
-    FAVORITES,
-    ADDRESS,
-    ROUND(AVG(review.REVIEW_SCORE), 2) as SCORE
-from REST_INFO info
-join REST_REVIEW review on info.REST_ID = review.REST_ID
-group by info.REST_ID
-having LEFT(info.ADDRESS, 2) = '서울'
-order by AVG(REVIEW_SCORE) desc, FAVORITES desc;
+-- 코드를 입력하세요
+SELECT
+    info.REST_ID AS REST_ID,
+    info.REST_NAME AS REST_NAME,
+    info.FOOD_TYPE AS FOOD_TYPE,
+    info.FAVORITES AS FAVORITES,
+    info.ADDRESS AS ADDRESS,
+    ROUND(AVG(REVIEW_SCORE), 2) AS SCORE
+FROM REST_INFO info
+JOIN REST_REVIEW review ON review.REST_ID = info.REST_ID
+WHERE info.ADDRESS LIKE '서울%'
+GROUP BY info.REST_ID, info.REST_NAME, info.FOOD_TYPE, info.FAVORITES, info.ADDRESS
+ORDER BY AVG(REVIEW_SCORE) DESC, FAVORITES DESC;
